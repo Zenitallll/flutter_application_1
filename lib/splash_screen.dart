ed35d3397772045c14d6ec444eda289554599e1a
Zenitallll <askararrafi23@gmail.com>
2026-09-17 12:00:17 +0700

    tugas

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:estron/service/shared_preference.dart';
+import 'package:estron/pages/login_screen.dart';
+import 'package:estron/navigator.dart';
 
-class SplashScreenDay15 extends StatefulWidget {
-  const SplashScreenDay15({super.key});
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
 
   @override
-  State<SplashScreenDay15> createState() => _SplashScreenDay15State();
+  State<SplashScreen> createState() => _SplashScreen();
 }
 
-class _SplashScreenDay15State extends State<SplashScreenDay15> {
+class _SplashScreen extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    goToLogin();
+  }
+
+  void goToLogin() async {
+    await Future.delayed(Duration(seconds: 3));
+    if (PreferenceHandler.isLogin == true) {
+      context.pushAndRemoveAll(HalamanTerimaKasih(email: "email"));
+    } else {
+      context.pushAndRemoveAll(LoginScreenDay15());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: Center(child: Image.asset('assets/splash.png')),
+        padding: const EdgeInsets.all(28.0),
+        child: Center(child: Image.asset("assets/images/sawit.jpeg")),
       ),
     );
   }
